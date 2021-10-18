46d36d1f8235c3f9980a06c805656f2585ef9a36
kmtran333 <kmtran333@gmail.com>
2021-10-18 13:55:34 -0700

    started hw3

---
+++ homework3/homework/models.py
@@ -0,0 +1,94 @@
+import torch
+import torch.nn.functional as F
+
+
+class CNNClassifier(torch.nn.Module):
+    class Block(torch.nn.Module):
+        def __init__(self, n_input, n_output, stride=1):
+            super().__init__()
+            self.net = torch.nn.Sequential(
+                torch.nn.Conv2d(n_input, n_output, kernel_size=3, padding=1, stride=stride),
+                torch.nn.ReLU(),
+                torch.nn.Conv2d(n_output, n_output, kernel_size=3, padding=1),
+                torch.nn.ReLU()
+            )
+
+        def forward(self, x):
+            return self.net(x)
+
+    def __init__(self, layers=[32, 64, 128], n_input_channels=3):
+        """
+        Your code here
+        """
+        super().__init__()
+        L = [
+            torch.nn.Conv2d(n_input_channels, 32, kernel_size=7, padding=3, stride=2),
+            torch.nn.ReLU()
+        ]
+        c = 32
+        for l in layers:
+            L.append(self.Block(c, l, stride=2))
+            c = l
+
+        self.network = torch.nn.Sequential(*L)
+        self.classifier = torch.nn.Linear(c, 6)
+
+    def forward(self, x):
+        """
+        Your code here
+        @x: torch.Tensor((B,3,64,64))
+        @return: torch.Tensor((B,6))
+        """
+
+        z = self.network(x)
+        z = self.classifier(z.mean(dim=[2, 3]))
+        return z
+
+
+class FCN(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        """
+        Your code here.
+        Hint: The FCN can be a bit smaller the the CNNClassifier since you need to run it at a higher resolution
+        Hint: Use up-convolutions
+        Hint: Use skip connections
+        Hint: Use residual connections
+        Hint: Always pad by kernel_size / 2, use an odd kernel_size
+        """
+        raise NotImplementedError('FCN.__init__')
+
+    def forward(self, x):
+        """
+        Your code here
+        @x: torch.Tensor((B,3,H,W))
+        @return: torch.Tensor((B,6,H,W))
+        Hint: Apply input normalization inside the network, to make sure it is applied in the grader
+        Hint: Input and output resolutions need to match, use output_padding in up-convolutions, crop the output
+              if required (use z = z[:, :, :H, :W], where H and W are the height and width of a corresponding strided
+              convolution
+        """
+        raise NotImplementedError('FCN.forward')
+
+
+model_factory = {
+    'cnn': CNNClassifier,
+    'fcn': FCN,
+}
+
+
+def save_model(model):
+    from torch import save
+    from os import path
+    for n, m in model_factory.items():
+        if isinstance(model, m):
+            return save(model.state_dict(), path.join(path.dirname(path.abspath(__file__)), '%s.th' % n))
+    raise ValueError("model type '%s' not supported!" % str(type(model)))
+
+
+def load_model(model):
+    from torch import load
+    from os import path
+    r = model_factory[model]()
+    r.load_state_dict(load(path.join(path.dirname(path.abspath(__file__)), '%s.th' % model), map_location='cpu'))
+    return r
---
+++ homework3/homework/train_cnn.py
@@ -0,0 +1,92 @@
+from .models import CNNClassifier, save_model
+from .utils import ConfusionMatrix, load_data, LABEL_NAMES
+import torch
+import torchvision
+import torch.utils.tensorboard as tb
+
+
+def train(args):
+    from os import path
+    model = CNNClassifier()
+
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    model.to(device)
+
+    train_logger, valid_logger = None, None
+    if args.log_dir is not None:
+        train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train'))
+        valid_logger = tb.SummaryWriter(path.join(args.log_dir, 'valid'))
+
+    """
+    Your code here, modify your HW1 code
+
+    """
+
+    # optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=0.9, weight_decay=1e-4)
+    optimizer = torch.optim.Adam(model.parameters())
+
+    if args.schedule_lr:
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience=50)
+
+    loss = torch.nn.CrossEntropyLoss()
+    global_step = 0
+
+    for epoch in range(args.n_epochs):
+        model.train()
+        train_accuracy = []
+        loss_data = []
+
+        for data, label in train_data:
+            if device is not None:
+                data, label = data.to(device), label.to(device)
+
+            o = model(data)
+            loss_val = loss(o, label)
+
+            train_accuracy.append(accuracy(o, label).detach().cpu().numpy())
+            loss_data.append(loss_val.detach().cpu().numpy())
+
+            train_logger.add_scalar('loss', float(loss_val.detach().cpu().numpy()), global_step=global_step)
+
+            optimizer.zero_grad()
+            loss_val.backward()
+            optimizer.step()
+
+            global_step += 1
+
+        train_logger.add_scalar('accuracy', np.mean(train_accuracy), global_step=global_step)
+
+        model.eval()
+        valid_accuracy = []
+        for data, label in valid_data:
+            if device is not None:
+                data, label = data.to(device), label.to(device)
+            o = model(data)
+            valid_accuracy.append(accuracy(o, label).detach().cpu().numpy())
+
+        if args.schedule_lr:
+            train_logger.add_scalar('lr', optimizer.param_groups[0]['lr'], global_step=global_step)
+            scheduler.step(np.mean(valid_accuracy))
+
+        valid_logger.add_scalar('accuracy', np.mean(valid_accuracy), global_step=global_step)
+
+        print(epoch, np.mean(train_accuracy), np.mean(valid_accuracy))
+    save_model(model)
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--log_dir')
+    # Put custom arguments here
+    parser.add_argument('--n_epochs', type=int, default=50)
+    parser.add_argument('--batch', type=int, default=128)
+    parser.add_argument('--lr', type=float, default=1e-3)
+    parser.add_argument('-sl', '--schedule_lr', action='store_true')
+
+    args = parser.parse_args()
+
+    train_data = load_data('data/train')
+    valid_data = load_data('data/valid')
+    train(args)
